dc1ef4de207702554c5fe87d82dc77347e6e0a96
Fernando Siles <fernando.siles@impar.com.br>
2021-10-26 23:16:31 -0300

    Initial commit

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface IMainProps {
+
+}
+
+const Main: React.FC<IMainProps> = (props: IMainProps) => {
+
+    return <>
+        <header></header>
+        <main>
+            <div>
+            </div>
+        </main>
+        <footer></footer>
+    </>
+}
+
+export default Main;
