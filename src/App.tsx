ce8d91538b7aa37de1a75e286ca66565b935cbe4
Fernando Siles <fdearaujosiles@gmail.com>
2021-12-08 07:50:58 -0300

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,21 @@
-import styles from './App.module.scss';
 import React from 'react';
-import Home from './components/Home/Home';
-import Water from './components/Water/Water';
-import Finances from './components/Finances/Finances';
+import './App.css';
 
-import { Button, CssBaseline } from '@nextui-org/react';
-import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom';
-
-const App: React.FC = () => {
-
-    return <Router>
-        <CssBaseline></CssBaseline>
-        <nav className={styles.Nav}>
-            <span className={styles.Title} >Tena</span>
-            <div className={styles.Links} >
-                <Link className={styles.Link} to="/">Home</Link>
-                <Link className={styles.Link} to="/water">Health</Link>
-                <Link className={styles.Link} to="/finances">Finances</Link>
-            </div>
-        </nav>
-        <div className={styles.Content} >
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/water" component={Water} />
-                <Route exact path="/finances" component={Finances} />
-            </Switch>
-        </div>
-    </Router>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
